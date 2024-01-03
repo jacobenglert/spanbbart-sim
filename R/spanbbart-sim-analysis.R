854de3f3fbe4663ea64c9b1aff3c644664f63f58
Jacob Englert <57627602+jacobenglert@users.noreply.github.com>
2024-01-03 13:49:33 -0500

    add figures from 02JAN2024 simulaltion study

--- R/spanbbart-sim-analysis.R
+++ R/spanbbart-sim-analysis.R
@@ -6,6 +6,7 @@
 
 # Load Packages -----------------------------------------------------------
 library(tidyverse)
+library(patchwork)
 
 
 # Load Results ------------------------------------------------------------
@@ -23,71 +24,61 @@ stats <- lapply(results, '[[', 'stats') |>
   group_by(param) |>
   summarise(across(c(bias, coverage, rmse), list(mean, lower, upper)))
 
+
+# Summarize First Order ALE -----------------------------------------------
+
 # First-order ALE
 ale1 <- lapply(results, '[[', 'ale1') |>
   bind_rows() |>
-  mutate(x_idx = row_number(), .by = ID) |>
-  #mutate(x = round(x, .1)) |>
   summarise(across(c(est, lcl, ucl, truth), mean), .by = c(x, var))
 
 ale1 |>
+  filter(!(x %in% c(min(x), max(x))), .by = var) |>
   ggplot(aes(x = x, ymin = lcl, ymax = ucl)) +
-  geom_line(aes(y = est)) +
-  geom_line(aes(y = truth), col = 'red') +
+  geom_hline(yintercept = 0, lty = 2, color = 'gray') +
+  geom_line(aes(y = est, col = 'Estimate')) +
+  geom_line(aes(y = truth, col = 'Truth'), lty = 2) +
   geom_ribbon(alpha = 0.4) +
-  geom_hline(yintercept = 0, lty = 2) +
-  facet_wrap(~factor(var, levels = colnames(x2)), ncol = 5, scales = 'free_y') +
+  facet_wrap(~factor(var, levels = unique(ale1$var)), ncol = 5, scales = 'free_y') +
   theme_bw() +
+  theme(legend.position = 'bottom') +
+  scale_color_manual(values = c("Truth" = "red", "Estimate" = "black")) +
   labs(title = 'ALE Plots of First-Order Main Effects',
        x = 'Predictor Value',
-       y = 'ALE')
+       y = 'ALE',
+       color = 'Type') +
+  guides(color = guide_legend(override.aes = list(lty = c(1, 2))))
+ggsave(here::here('Figures', sim_date,'ale-first-order.png'), width = 6.5, height = 5)
 
 
+# Summarize Second Order ALE ----------------------------------------------
 
 # Second-order ALE
 ale2 <- lapply(results, '[[', 'ale2') |>
   bind_rows() |>
-  mutate(x_idx = row_number(), .by = ID) |>
-  # mutate(x1 = round(x1, 2),
-  #        x2 = round(x2, 2)) |>
   summarise(across(c(est, lcl, ucl, truth, est_main, truth_main), mean), 
             .by = c(x1, x2, w1, w2, h1, h2, var1, var2)) |>
-  arrange(var1, var2, x1, x2)
-
-# ale2df <- ale2 |>
-#   group_by(var1, var2) |>
-#   group_split() |>
-#   lapply(\(x){
-#     x1 <- unique(x$x1)
-#     x2 <- unique(x$x2)
-#     w1 <- c(diff(x1)[1], diff(x1)) / 2
-#     w2 <- c(rev(abs(diff(rev(x1)))), rev(abs(diff(x1)))[1]) / 2
-#     h1 <- c(diff(x2)[1], diff(x2)) / 2
-#     h2 <- c(rev(abs(diff(rev(x2)))), rev(abs(diff(x2)))[1]) / 2
-#     
-#     x |>
-#       mutate(w1 = rep(w1, each = length(unique(x$x2))),
-#              w2 = rep(w2, each = length(unique(x$x2))),
-#              h1 = rep(h1, times = length(unique(x$x1))),
-#              h2 = rep(h2, times = length(unique(x$x1))))
-#     
-#   }) |>
-#   bind_rows()
+  arrange(var1, var2, x1, x2) |>
+  filter(!(x1 %in% c(min(x1), max(x1)) | x2 %in% c(min(x2), max(x2))), .by = c(var1, var2))
 
 
 # Estimate (without Main Effects)
 limits <- c(min(ale2$truth, ale2$est), max(ale2$truth, ale2$est))
 ale2_est <- ale2 |>
   ggplot(aes(xmin = x1 - w1, xmax = x1 + w2, ymin = x2 - h1, ymax = x2 + h2)) +
-  geom_rect(aes(fill = est)) +
+  geom_rect(aes(fill = est), show.legend = FALSE) +
   theme_bw() +
   coord_fixed() +
-  scale_x_continuous(expand = c(0,0)) +
+  scale_x_continuous(expand = c(0,0), breaks = seq(0,1,0.25)) +
   scale_y_continuous(expand = c(0,0)) +
   scale_fill_gradient2(low = 'blue', high = 'red', limits = limits) +
   facet_grid(var2 ~ var1) +
   theme_bw() +
-  labs(title = 'Estimated Second-Order ALEs (excluding Main Effects)',
+  theme(axis.text.x = element_text(angle = 270),
+        plot.title = element_text(hjust = 0.5),
+        plot.subtitle = element_text(hjust = 0.5)) +
+  labs(title = 'Estimated Second-Order ALEs',
+       subtitle = 'Excluding Main Effects',
        x = 'Predictor 1 Value',
        y = 'Predictor 2 Value',
        fill = 'ALE')
@@ -98,15 +89,23 @@ ale2_true <- ale2 |>
   geom_rect(aes(fill = truth)) +
   theme_bw() +
   coord_fixed() +
-  scale_x_continuous(expand = c(0,0), breaks = seq(0,1,0.5)) +
+  scale_x_continuous(expand = c(0,0), breaks = seq(0,1,0.25)) +
   scale_y_continuous(expand = c(0,0)) +
   scale_fill_gradient2(low = 'blue', high = 'red', limits = limits) +
   facet_grid(var2 ~ var1) +
   theme_bw() +
-  labs(title = 'True Second-Order ALEs (excluding Main Effects)',
+  theme(axis.text.x = element_text(angle = 270),
+        plot.title = element_text(hjust = 0.5),
+        plot.subtitle = element_text(hjust = 0.5)) +
+  labs(title = 'True Second-Order ALEs',
+       subtitle = 'Excluding Main Effects',
        x = 'Predictor 1 Value',
        y = 'Predictor 2 Value',
        fill = 'ALE')
+
+ale2_est + ale2_true + plot_layout(guides = 'collect')
+ggsave(here::here('Figures', sim_date,'ale-second-order.png'), width = 6.5, height = 4)
+
 
 # Estimate (with Main Effects)
 limits <- c(min(ale2$truth_main, ale2$est_main), max(ale2$truth_main, ale2$est_main))
@@ -115,12 +114,16 @@ ale2_est_main <- ale2 |>
   geom_rect(aes(fill = est_main)) +
   theme_bw() +
   coord_fixed() +
-  scale_x_continuous(expand = c(0,0)) +
+  scale_x_continuous(expand = c(0,0), breaks = seq(0,1,0.25)) +
   scale_y_continuous(expand = c(0,0)) +
   scale_fill_gradient2(low = 'blue', high = 'red', limits = limits) +
   facet_grid(var2 ~ var1) +
   theme_bw() +
-  labs(title = 'Estimated Second-Order ALEs (including Main Effects)',
+  theme(axis.text.x = element_text(angle = 270),
+        plot.title = element_text(hjust = 0.5),
+        plot.subtitle = element_text(hjust = 0.5)) +
+  labs(title = 'Estimated Second-Order ALEs',
+       subtitle = 'Including Main Effects',
        x = 'Predictor 1 Value',
        y = 'Predictor 2 Value',
        fill = 'ALE')
@@ -131,16 +134,20 @@ ale2_true_main <- ale2 |>
   geom_rect(aes(fill = truth_main)) +
   theme_bw() +
   coord_fixed() +
-  scale_x_continuous(expand = c(0,0), breaks = seq(0,1,0.2)) +
+  scale_x_continuous(expand = c(0,0), breaks = seq(0,1,0.25)) +
   scale_y_continuous(expand = c(0,0)) +
   scale_fill_gradient2(low = 'blue', high = 'red', limits = limits) +
   facet_grid(var2 ~ var1) +
   theme_bw() +
-  labs(title = 'True Second-Order ALEs (including Main Effects)',
+  theme(axis.text.x = element_text(angle = 270),
+        plot.title = element_text(hjust = 0.5),
+        plot.subtitle = element_text(hjust = 0.5)) +
+  labs(title = 'True Second-Order ALEs',
+       subtitle = 'Including Main Effects',
        x = 'Predictor 1 Value',
        y = 'Predictor 2 Value',
        fill = 'ALE')
 
-library(patchwork)
-ale2_est + ale2_true
-ale2_est_main + ale2_true_main
+# Plot posterior mean and truth side-by-side
+ale2_est_main + ale2_true_main + plot_layout(guides = 'collect')
+ggsave(here::here('Figures', sim_date,'ale-second-order-w-main.png'), width = 6.5, height = 4)
